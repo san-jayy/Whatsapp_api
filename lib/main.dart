dadff127b33637f81b642695993684d964685884
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-04-27 17:55:47 +0530

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp_api/views/homepage.dart';
+import 'package:whatsapp_api/views/Homepage.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
